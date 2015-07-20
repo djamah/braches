8858d575b20c5d61d7ffe64c2352d373744b794d
djamah <i@djamah.org>
2015-07-20 19:29:01 +0300

    fix upload

--- routes/send_breach.js
+++ routes/send_breach.js
@@ -6,21 +6,21 @@ module.exports = function (req, res) {
     console.log(req.body, 'req');
     console.log(req.files.file, 'req');
 
-    var oldPath = req.files.file.path;
-    var newPath = 'public/uploads/'+Date.now()+req.files.file.originalFilename;
-
-    fs.readFile(oldPath , function(err, data) {
-        fs.writeFile(newPath, data, function(err) {
-            fs.unlink(oldPath, function(){
-                if(err) throw err;
-                res.send("File uploaded to: " + newPath);
+    var breach = new Breach(JSON.parse(req.body.form));
+    if(req.files.file){
+        var oldPath = req.files.file.path;
+        var newPath = 'public/uploads/'+Date.now()+req.files.file.originalFilename;
+        fs.readFile(oldPath , function(err, data) {
+            fs.writeFile(newPath, data, function(err) {
+                fs.unlink(oldPath, function(){
+                    if(err) throw err;
+                    res.send("File uploaded to: " + newPath);
+                });
             });
         });
-    });
-    var breach = new Breach(JSON.parse(req.body.form));
+        breach.file = newPath.substr(6);
+    }
     breach.date = new Date();
-    breach.file = newPath.substr(6);
-    console.log(breach.file);
     breach.publish = false;
     breach.fixed = false;
     breach.save(function (err, item) {
